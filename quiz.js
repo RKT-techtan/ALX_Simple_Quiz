41ea8e31bbd3c77fe7386fd54b22de849a07f1e8
RKT-tehtan <fandfilian098@gmail.com>
2025-01-18 16:47:26 +0300

    implementing the JavaScript functionality for a quiz application

--- quiz.js
+++ quiz.js
@@ -1,31 +1,17 @@
-//(1.)////////tick
 function checkAnswer() {
+
     const correctAnswer = "4";
-    //(2.)tick
-    //(a)
+
     const answer = document.querySelector('name="quiz"');
     console.log()
 
-    //example//
-const paras = document.getElementsByClassName('p');
-console.log(paras);
-console.log(paras[1]);
-    //example end//
-
-    //(b.))////
     let userAnswer = answer.value;
-//3///////tick
         if (userAnswer === correctAnswer) {
-          feedback.textContent = "Correct! Well done."; 
-
-          //change the textContent of the 
-          //element with the ID feedback to "Correct! Well done.".
-
+          const feedback = document.getElementById("Correct! Well done.");
         } else {
-          feedback.textContent = "That's incorrect. Try again!";
+          const feedback = document.getElementById("That's incorrect. Try again!");
         }
-//(4.)/////tick
-document.getElementById = 'submit-answer';
 
-submitButton.addEventListener("click", checkAnswer);
+        const submitButton = document.getElementById('submit-answer'); 
+        submitButton.addEventListener("click", checkAnswer);
 }
